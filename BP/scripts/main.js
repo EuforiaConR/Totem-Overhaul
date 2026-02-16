86c5318712d038909ab71db44e93df2f1aa1860a
GGeokiller <geokilleroficial@gmail.com>
2026-02-16 01:45:18 -0700

    totem effects registry

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -6,6 +6,7 @@ import {
   MolangVariableMap,
 } from "@minecraft/server";
 import { TotemRegistry } from "./totemRegistry.js";
+import { TotemEffectRegistry } from "./TotemEffects.js";
 import "./items/!index.js";
 
 function consumeItem(player, slot, amount = 1) {
